9a84f4bb1022d97a6df0a50c04085a692bd5162a
Michal Hiterski <michal.hiterski@comarch.com>
2022-12-05 22:28:46 +0100

    5**

--- 5/5.ts
+++ 5/5.ts
@@ -1,17 +1,24 @@
-import { fetchData, filter, flatMap, log, map, parseData, pipe, sum } from "../utils";
+import { fetchData, log, map, parseData, pipe } from "../utils";
 
 // 1:1 5:2 9:3 13:4
 // 3a + b = 9// b = 9 -3a
 // 4a + b = 13 // 4a +9 -3a = 13 // a = 4 // b = 9-12 = -3// y = 4x -3
-// 9
+
 const r19 = Array.from({ length: 9 }, (_, i) => i + 1);
+
 const translateXY = array => array[0].map((_, colIndex) => array.map(row => row[colIndex]));
 
-const getOnStack = (crates, [cratesN, from, to], stack = []): [any, any, any] =>
+// tworzy stos krat - dzwig podnosi
+const getOnStack = ([crates, [cratesN, from, to], stack = []]): [any, any, any] =>
     cratesN === 0 ?
         [crates, [cratesN, from, to], stack] :
-        getOnStack(crates.map((c, i) => i === from - 1 ? c.slice(0, -1) : c), [cratesN - 1, from, to], [...stack, crates[from - 1][crates[from - 1].length - 1]]);
+        getOnStack(
+            [crates.map((c, i) => i === from - 1 ? c.slice(0, -1) : c),
+            [cratesN - 1, from, to],
+            [...stack, crates[from - 1][crates[from - 1].length - 1]]]
+        );
 
+// oproznia stos krat - dzwig  opuszcza
 const getFromStack$ = ([crates, [cratesN, from, to], stack]) =>
     stack.length === 0 ?
         crates :
@@ -21,29 +28,29 @@ const getFromStack$ = ([crates, [cratesN, from, to], stack]) =>
             stack.slice(1)]
         );
 
+// iterowanie po instrukcjach - dzwig przesuwa 
 const move$ = ([crates, instructions]) => instructions.length ?
-    move$([getFromStack$(getOnStack(crates, instructions[0])), instructions.slice(1)]) :
+    move$([pipe([crates, instructions[0]], getOnStack, getFromStack$), instructions.slice(1)]) :
     crates;
 
 //*
-
 
 pipe(
     './5/5p.txt',
     fetchData,
     parseData,
     data => [
-        data.slice(0, 8)
+        data.slice(0, 8) // tylko pierwsze 9 wiersze
             .reverse()
             .map(d => d.split(''))
-            .map((_, i, arr) => r19.map(x => arr[i][x * 4 - 3])),
-        data.slice(10)
+            .map((_, i, arr) => r19.map(x => arr[i][x * 4 - 3])), // po indekach biore litery zgodnie z wyliczona y = 4x-3 funkcja liniowa
+        data.slice(10) // instrukcje od 10 do konca
     ],
     data => [
-        translateXY(data[0]).map(column => column.filter(c => c !== ' ')),
+        translateXY(data[0]).map(column => column.filter(c => c !== ' ')), // translacja osi x na y
         data[1].map(s => s.split(' ').map(d => +d).filter(c => !!c))
     ],
-    move$,
+    move$, // dzwiganie krat
     map((d: string[]) => d[d.length - 1]),
     data => data.join(''),
     log
@@ -61,7 +68,7 @@ const getFromStack$$ = ([crates, [cratesN, from, to], stack]) =>
             stack.slice(0, -1)]
         );
 const move$$ = ([crates, instructions]) => instructions.length ?
-    move$$([getFromStack$$(getOnStack(crates, instructions[0])), instructions.slice(1)]) :
+    move$$([pipe([crates, instructions[0]], getOnStack, getFromStack$$), instructions.slice(1)]) :
     crates;
 
 pipe(
